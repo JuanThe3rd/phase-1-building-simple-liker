26798eb8e1e0bc9d0a2ece37529be73c32a37a62
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-27 15:14:28 -0700

    complete lab

--- main.js
+++ main.js
@@ -4,8 +4,26 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeBtns = document.querySelectorAll('.like');
 
+for (let i = 0; i < 5; i++) {
+  likeBtns[i].addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        likeBtns[i].innerHTML = `<li class='like'>Like! <span class="like-glyph activated-heart">${FULL_HEART}</span></li>`;
+      })
+      .catch((error) => {
+        const errorAlert = document.getElementById('modal');
+        const errorAlertmessage = document.getElementById('modal-message');
+        errorAlertmessage.textContent = error;
 
+        errorAlert.classList.remove('hidden');
+        setTimeout(() => {
+          errorAlert.classList.add('hidden')
+        }, 3000);
+      })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
